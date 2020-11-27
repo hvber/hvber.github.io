57a0c8aa4cccef3ae128b15a7ad8b4f02ea0c478
Sebastian <51960484+hvber@users.noreply.github.com>
2020-11-27 11:14:27 +0100

    Add files via upload

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,7 +1,14 @@
-var menu = document.querySelector('.menu');
-var hamburger = document.getElementById('hamburger');
-var div = document.querySelector('#background');
+var menu = document.querySelector('.menu'),
+    hamburger = document.getElementById('hamburger'),
+    div = document.querySelector('#background');
 
+function menuToggle(){
+    if(menu.classList.contains('active')){
+        hamburger.classList.toggle('active');
+        menu.classList.toggle('active');
+        div.classList.toggle('active');
+    }
+}
 
 if(hamburger){
     hamburger.addEventListener('click', ()=>{
@@ -11,7 +18,14 @@ if(hamburger){
     });
 }
 div.addEventListener('click', ()=>{
-    if(div.classList.contains('active')){
+    if(menu.classList.contains('active')){
+        hamburger.classList.toggle('active');
+        menu.classList.toggle('active');
+        div.classList.toggle('active');
+    }
+});
+menu.addEventListener('click', ()=>{
+    if(menu.classList.contains('active')){
         hamburger.classList.toggle('active');
         menu.classList.toggle('active');
         div.classList.toggle('active');
